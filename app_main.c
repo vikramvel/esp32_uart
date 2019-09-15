459b503095fb049253ec5ec72acb033df27b3838
Vikram Vel <vv@interelme.com>
2019-09-15 10:58:40 +0400

    Initial Commit

---
+++ app_main.c
@@ -0,0 +1,40 @@
+/*
+** @source __MAIN_C__
+**
+** __ShortDescription__
+**
+** @author Copyright (C) 2019 Author: Vikramvel
+** @version 2019
+**
+********************************************************************/
+#include "main.h"
+
+/* Function to intialize uart_config
+ * baud_rate,
+ * data_bits,
+ * parity,
+ * stop_bits,
+ * flow_ctrl
+ */
+void init_uart(void) 
+{
+
+}
+
+/* API for tx of UART data */
+int txData(const char* data)
+{
+
+}
+
+/* Task for receiving data from UART */
+static void rx_task(void *atx)
+{
+
+}
+
+/* Main Function */
+void app_main()
+{
+
+}
