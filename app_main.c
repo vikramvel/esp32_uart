c57118ea93fd14c2d72e79f9b401a872456536f2
Vikram Vel <vv@interelme.com>
2019-09-16 09:30:41 +0400

    Second Commit: Initialzed functions, added UART Tx API

--- app_main.c
+++ app_main.c
@@ -18,13 +18,25 @@
  */
 void init_uart(void) 
 {
-
+    const uart_config_t uart_config = {
+        .baud_rate  = 115200,
+        .data_bits  = UART_DATA_8_BITS,
+        .parity     = UART_PARITY_DISABLE,
+        .stop_bits  = UART_STOP_BITS_1,
+        .flow_ctrl  = UART_HW_FLOWCTRL_DISABLE
+    };
+    uart_param_config(uart_num, &uart_config);
+    ESP_ERROR_CHECK(uart_set_pin(uart_num, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
+    // We won't use a buffer for sending data.
+    uart_driver_install(uart_num, RX_BUF_SIZE * 2, 0, 0, NULL, 0);
 }
 
 /* API for tx of UART data */
 int txData(const char* data)
 {
-
+    const int len = strlen(data);
+    const int txBytes = uart_write_bytes(uart_num, data, len);
+    return txBytes;
 }
 
 /* Task for receiving data from UART */
--- main.h
+++ main.h
@@ -34,10 +34,43 @@
 /* ==================== function prototypes =========================== */
 /* ==================================================================== */
 
+/**
+ * @brief UART intialization
+ *
+ * API to assign the neccessary UART intialization params.
+ * 
+ *
+ * @param  void
+ * 
+ * @return void
+ */
 void init_uart(void);
 
+/**
+ * @brief Transmission of data through UART
+ *
+ * Function to send the UART tx bytes from UART number initialized.
+ * Primary NULL check should be done for the data to be sent across.
+ *
+ * @param  data to be sent.
+ * 
+ * @return 
+ *  - (-1) Parameter error
+ *  - OTHERS (>=0) The number of bytes pushed to the TX FIFO
+ */
 int txData(const char* data);
 
+/**
+ * @brief   Function to receive RX bytes received in ESP.
+ *
+ * This function is to retrieve the data received by UART and saved in Rx FIFO,
+ *
+ * @param   atx     pvParameter NULL is passed as parameter
+ *
+ * @return 
+ *  - (-1) Error
+ *  - OTHERS (>=0) The number of bytes read from UART FIFO
+ */
 static void rx_task(void *atx);
 
 #endif
